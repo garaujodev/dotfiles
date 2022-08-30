0ee59f7151f9e7ceb51d12de01c8a0e850fdfeda
Gustavo Araujo <garaujodev@gmail.com>
2022-08-30 18:53:34 -0300

    Change `install.sh` script to install binaries

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
-files=( gitconfig gitignore_global pryrc gemrc alias zshrc vimrc tigrc)
+files=(gitconfig gitignore_global pryrc gemrc alias zshrc vimrc tigrc)
+
+binaries=(git-clear)
 
 for filename in ${files[@]}
 do
   [[ -s $HOME/.$filename ]] && rm $HOME/.$filename
   ln -sf $PWD/$filename ~/.$filename
+done
+
+for binary in ${binaries[@]}
+do
+  [[ -s $HOME/.local/bin/$binary ]] && rm $HOME/.local/bin/$binary
+  ln -sf $PWD/$binary ~/.local/bin/$binary
 done
 
 source ~/.zshrc
